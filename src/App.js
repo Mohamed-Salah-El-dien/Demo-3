ab37de3979e05f0e90cae50b8c0e67588d3514d3
Mohamed Salah <mohamedelsebaey513@gmail.com>
2023-04-13 23:58:18 +0200

    minor adjustment

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route index path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="models" element={<Models />} />
-        <Route path="testimonials" element={<TestimonialsPage />} />
-        <Route path="team" element={<Team />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/models" element={<Models />} />
+        <Route path="/testimonials" element={<TestimonialsPage />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
